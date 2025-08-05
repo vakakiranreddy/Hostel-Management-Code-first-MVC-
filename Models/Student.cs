9512551c5bbbcc8814563467c500e29041aa8c9c
Kiran <kiranreddyvaka4@gmail.com>
2025-08-05 17:31:02 +0530

    added

---
+++ Models/Student.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HostelManagement.Models
+{
+    public class Student
+    {
+        [Key]
+        public int Id { get; set; }
+      
+        public string Name { get; set; }
+
+        public int RoomNumber { get; set; }
+
+        public string Department { get; set; }
+
+        public DateTime AdmissionDate { get; set; }
+    }
+}
---
+++ Models/StudentContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace HostelManagement.Models
+{
+    public class StudentContext:DbContext
+        
+    {
+        public DbSet<Student> Students { get; set; }
+
+        public StudentContext(DbContextOptions<StudentContext> options) : base(options) { }
+
+        
+
+    }
+}
